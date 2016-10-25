894a4b95f31727665ebedaaaedfa3b5bb71133b5
maxwelllincoln <maxwell.lincoln@austin.utexas.edu>
2016-10-25 10:54:12 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function changeColor() {
+
+    var orangeSection = document.getElementsByClassName('section-container ut-color burnt-orange-100');
+
+    for (var i = 0; i < orangeSection.length; i++) {
+
+        orangeSection[i].style.backgroundColor="blue";
+
+    }
+}
